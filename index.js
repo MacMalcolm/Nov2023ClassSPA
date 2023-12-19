a66f305b8bd686049a334ef86b88805cf7e874d4
Mckinzie Gilbert-Malcolm <Mac.Malcolm88@gmail.com>
2023-12-18 18:41:25 -0600

    Adds after render function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store";
 import Navigo from "navigo";
-import { capitalize } from "lodash";
+import { after, capitalize } from "lodash";
 
 const router = new Navigo("/");
 
@@ -13,26 +13,31 @@ ${Main(state)}
 ${Footer()}
 `;
 
-router.updatePageLinks();
+  router.updatePageLinks();
+  afterRender();
+}
+
+function afterRender() {
+//add menu toggle to bars icon in nav bar
+document.querySelector(".fa-bars").addEventListener("click", () => {
+  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+});
 }
 
 router
-.on({
-  "/": () => render(),
-  ":view": (params) => {
-    let view = capitalize(params.data.view);
-    if (view in store) {
-      render(store[view]);
-    } else {
-      render(store.Viewnotfound);
-      console.log(`View ${view} not defined`);
+  .on({
+    "/": () => render(),
+    ":view": params => {
+      let view = capitalize(params.data.view);
+      if (view in store) {
+        render(store[view]);
+      } else {
+        render(store.Viewnotfound);
+        console.log(`View ${view} not defined`);
+      }
     }
-  },
-})
-.resolve();
+  })
+  .resolve();
 //render();
 
-// add menu toggle to bars icon in nav bar
-// document.querySelector(".fa-bars").addEventListener("click", () => {
-//   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-// });
+
