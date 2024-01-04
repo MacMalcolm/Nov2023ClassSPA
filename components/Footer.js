b62add0414a9929125be99034ea296a903e1a743
Mckinzie Gilbert-Malcolm <Mac.Malcolm88@gmail.com>
2024-01-03 20:53:03 -0600

    Wraps up pizza place API

--- components/Footer.js
+++ components/Footer.js
@@ -3,6 +3,5 @@ import html from "html-literal";
 export default state => html`
   <footer>
     &copy; 2020 <a href="https://savvycoders.com/">Savvy Coders</a>
-    The weather in ${state.weather.city} is ${state.weather.description}.
   </footer>
 `;
